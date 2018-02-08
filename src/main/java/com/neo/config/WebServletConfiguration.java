26dcc75adc3bf681b2684126debfe1f24e853e2a
Vaibhav Kharche <vaibhavmkharche@gmail.com>
2018-02-08 19:28:47 +0530

    Adds spring boot feature

---
+++ src/main/java/com/neo/config/WebServletConfiguration.java
@@ -0,0 +1,30 @@
+package com.neo.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebServletConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		// create webApplicationContext
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.register(ApplicationConfiguration.class);
+		
+		//	manage lifecycle of root Application context
+		container.addListener(new ContextLoaderListener(webContext));
+		
+		//	register and map dispatcher servlet
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(webContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+		
+	}
+
+}
